064c6cc3258f41393a7778df903113cec5ac578e
Nathan Gray <nathan.gray@gordon.edu>
2016-04-26 01:55:25 -0400

    Get Place Names
    App generates a list of names of restaurants within 5000 meters.

--- Places/app/src/main/java/com/app/places/places/MainActivity.java
+++ Places/app/src/main/java/com/app/places/places/MainActivity.java
@@ -1,13 +1,159 @@
 package com.app.places.places;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity
+        implements OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,
+        com.google.android.gms.location.LocationListener {
+
+    private GoogleApiClient mGoogleApiClient; // Connects to Google's API
+    private Location currentLocation; // Stores the most recent location data
+    private String TAG = "MainActivity"; // Used to identify error messages
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the API client
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    // Called when the Client is initialized
+    protected void onStart() {
+        Log.i(TAG, "Client Connecting");
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    // Called when Client is finished
+    protected void onStop() {
+        Log.i(TAG, "Client Disconnecting");
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // As soon as the Client connects, get the last location
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        // Create a location request for the automatically updating data
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(30000); // Poll every thirty seconds
+        request.setFastestInterval(30000);
+
+        // Create a special instance of the location provider
+        FusedLocationProviderApi fusedLocationProviderApi = LocationServices.FusedLocationApi;
+
+        // Request regular updates from the special location provider using the request object
+        fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, request, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection Suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    // Called every time the location provider gets new data
+    public void onLocationChanged(Location location) {
+        // Update the current location with the new data
+        currentLocation = location;
+
+        // If it's not null, get the latitude and longitude and get places
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            makePlacesRequest(lat, lon);
+        }
+    }
+
+    // Sends a request to Google Places API
+    private void makePlacesRequest(double lat, double lon) {
+
+        // URL components
+        String placesKey = "AIzaSyCl-8E28wKWpmbIEstGpjWoPrMPsBvYGXk";               // API key
+        int radius = 5000;                                                          // Search Radius
+        String location = lat + "," + lon;                                          // Location
+        String type = "restaurant";                                                 // Type of place
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"; // Base url
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+
+        // Add parameters to request
+        params.put("location", location);
+        params.put("radius", radius);
+        params.put("type", type);
+        params.put("key", placesKey);
+        client.get(url, params, new TextHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String res) {
+                        // called when response HTTP status is "200 OK"
+                        parseResponse(res);
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
+                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    }
+                }
+        );
+    }
+
+    // Parse the JSON data that is returned
+    private String parseResponse(String responseString) {
+        try {
+            JSONObject inJson = new JSONObject(responseString); // Initial Json response
+            JSONArray results = (JSONArray) inJson.get("results");
+            ArrayList<String> names = new ArrayList<String>();
+            for (int i = 0; i < results.length(); i ++) {
+                JSONObject place = (JSONObject) results.get(i);
+                String name = place.getString("name");
+                names.add(name);
+            }
+            System.out.println();
+        } catch (Exception e) {
+            Log.d("parseResponse()", "Unable to parse response");
+        }
+        return null;
     }
 }
+

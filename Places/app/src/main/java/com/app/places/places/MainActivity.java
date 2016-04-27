64024c052f701d7ddd3f69208308e15f604c636d
Nathan Gray <nathan.gray@gordon.edu>
2016-04-26 22:51:55 -0400

    Lists of Places
    Gets lists of data about three categories of places: food, fun, and
    shopping.

--- Places/app/src/main/java/com/app/places/places/MainActivity.java
+++ Places/app/src/main/java/com/app/places/places/MainActivity.java
@@ -30,6 +30,18 @@ public class MainActivity extends AppCompatActivity
     private Location currentLocation; // Stores the most recent location data
     private String TAG = "MainActivity"; // Used to identify error messages
 
+    private ArrayList<Place> food;
+    private String foodType = "restaurant|bakery|bar|cafe|meal_delivery|meal_takeaway";
+    private ArrayList<Place> fun;
+    private String funType = "amusement_park|aquarium|art_gallery|beauty_salon|bowling_alley|" +
+            "campground|casino|movie_rental|movie_theater|museum|night_club|park|spa|stadium|" +
+            "zoo|gym";
+    private ArrayList<Place> shopping;
+    private String shopType = "bicycle_store|book_store|jewelry_store|liquor_store|pet_store|" +
+            "shoe_store|shopping_mall|store|clothing_store|convenience_store|department_store|" +
+            "electronics_store|florist|furniture_store|grocery_or_supermarket|hardware_store|" +
+            "home_goods_store";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +78,14 @@ public class MainActivity extends AppCompatActivity
         // As soon as the Client connects, get the last location
         currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
+        // Get places data
+        float currLat = (float) currentLocation.getLatitude();
+        float currLon = (float) currentLocation.getLongitude();
+
+        makePlacesRequest(currLat, currLon, foodType);
+        makePlacesRequest(currLat, currLon, funType);
+        makePlacesRequest(currLat, currLon, shopType);
+
         // Create a location request for the automatically updating data
         LocationRequest request = LocationRequest.create();
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
@@ -93,26 +113,54 @@ public class MainActivity extends AppCompatActivity
     @Override
     // Called every time the location provider gets new data
     public void onLocationChanged(Location location) {
-        // Update the current location with the new data
-        currentLocation = location;
 
-        // If it's not null, get the latitude and longitude and get places
+        // If it's not null, get the latitude and longitude
         if (location != null) {
-            double lat = location.getLatitude();
-            double lon = location.getLongitude();
+            float currLat = (float) currentLocation.getLatitude();
+            float currLon = (float) currentLocation.getLongitude();
+            float newLat = (float) location.getLatitude();
+            float newLon = (float) location.getLongitude();
 
-            makePlacesRequest(lat, lon);
+            // Update places if the latitude and longitude are different enough from the current
+            // location
+            if (distFrom(currLat, currLon, newLat, newLon) > 500) {
+                // Update the current location with the new data
+                currentLocation = location;
+
+                // Get places data
+                makePlacesRequest(newLat, newLon, foodType);
+                makePlacesRequest(newLat, newLon, funType);
+                makePlacesRequest(newLat, newLon, shopType);
+            }
         }
     }
 
+    // Calculates the distance between two sets of coordinates
+    // Credit to http://stackoverflow.com/questions/837872/calculate-distance-in-meters-when-you-
+    // know-longitude-and-latitude-in-java
+    public float distFrom(float lat1, float lng1, float lat2, float lng2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        float dist = (float) (earthRadius * c);
+
+        return dist;
+    }
+
     // Sends a request to Google Places API
-    private void makePlacesRequest(double lat, double lon) {
+    private void makePlacesRequest(float lat, float lon, final String type) {
+
+        // List of places to return
+        ArrayList<Place> places = new ArrayList<Place>();
 
         // URL components
         String placesKey = "AIzaSyCl-8E28wKWpmbIEstGpjWoPrMPsBvYGXk";               // API key
         int radius = 5000;                                                          // Search Radius
         String location = lat + "," + lon;                                          // Location
-        String type = "restaurant";                                                 // Type of place
         String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"; // Base url
 
         AsyncHttpClient client = new AsyncHttpClient();
@@ -127,7 +175,15 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public void onSuccess(int statusCode, Header[] headers, String res) {
                         // called when response HTTP status is "200 OK"
-                        parseResponse(res);
+                        if (type == foodType) {
+                            food = parseResponse(res);
+                        }
+                        else if (type == funType) {
+                            fun = parseResponse(res);
+                        }
+                        else if (type == shopType) {
+                            shopping = parseResponse(res);
+                        }
                     }
 
                     @Override
@@ -139,17 +195,30 @@ public class MainActivity extends AppCompatActivity
     }
 
     // Parse the JSON data that is returned
-    private String parseResponse(String responseString) {
+    private ArrayList<Place> parseResponse(String responseString) {
         try {
             JSONObject inJson = new JSONObject(responseString); // Initial Json response
             JSONArray results = (JSONArray) inJson.get("results");
-            ArrayList<String> names = new ArrayList<String>();
+
+            ArrayList<Place> places = new ArrayList<Place>(); // List of Place objects to return
+
+            // Populate array of Places from places in response
             for (int i = 0; i < results.length(); i ++) {
-                JSONObject place = (JSONObject) results.get(i);
-                String name = place.getString("name");
-                names.add(name);
+                JSONObject placeObj = (JSONObject) results.get(i);
+                Place place = new Place();
+                if (placeObj.has("name")) {
+                    place.setName(placeObj.getString("name"));
+                }
+                if (placeObj.has("rating")) {
+                    place.setRating(placeObj.getDouble("rating"));
+                }
+                if (placeObj.has("vicinity")) {
+                    place.setAddress(placeObj.getString("vicinity"));
+                }
+                places.add(place);
             }
-            System.out.println();
+
+            return places;
         } catch (Exception e) {
             Log.d("parseResponse()", "Unable to parse response");
         }
---
+++ Places/app/src/main/java/com/app/places/places/Place.java
@@ -0,0 +1,32 @@
+package com.app.places.places;
+
+public class Place {
+
+    private String name;
+    private double rating;
+    private String address;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
